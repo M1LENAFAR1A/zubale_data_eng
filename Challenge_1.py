f60c673b54e354b79bea5b868bbd1999bb813519
Milena <84186837+M1LENAFAR1A@users.noreply.github.com>
2024-12-26 00:25:12 -0300

    update better code

--- Challenge_1.py
+++ Challenge_1.py
@@ -1,34 +1,24 @@
-import pandas as pd 
+import pandas as pd
 
-products_df = pd.read_csv('data/products.csv')
-orders_df = pd.read_csv('data/orders.csv')
+products = pd.read_csv('data/products.csv')
+orders = pd.read_csv('data/orders.csv')
 
-merged_df = pd.merge(
-    orders_df,         
-    products_df,      
-    left_on='product_id',  
-    right_on='id'         
-)
+merge_df = pd.merge(products, orders,left_on='id', right_on='product_id', suffixes=('_product', '_order'))
 
-merged_df['total_price'] = merged_df['quantity'] * merged_df['price']
+merge_df['total_price'] = merge_df['price'] * merge_df['quantity']
 
-final_df = merged_df[[
-    'created_date',   
-    'id_x',           
-    'name',           
-    'quantity',       
-    'total_price'     
-]]
-
-final_df.columns = [
-    'order_created_date',
-    'order_id',
-    'product_name',
+final_df = merge_df[[
+    'created_date',
+    'id_order',
+    'name',
     'quantity',
     'total_price'
-]
+]].rename(columns={
+    'created_date': 'order_created_date',
+    'id_order': 'order_id',
+    'name': 'product_name'
+})
 
 final_df.to_csv('results/order_full_information.csv', index=False)
 
 print("File 'order_full_information.csv' created successfully!")
-
