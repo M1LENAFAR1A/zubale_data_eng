3ad422de8831b2a73ac6504eb594c24e85b9c9fa
Milena <84186837+M1LENAFAR1A@users.noreply.github.com>
2024-12-26 01:40:27 -0300

    correct response

--- 2.py
+++ 2.py
@@ -4,38 +4,39 @@ import requests
 import json
 
 base = pd.read_csv('results/order_full_information.csv')
-base.rename(columns={"total_price": "total_price_br"})
-print(base.head())
-# try:
-#     with open('config.json') as config_file:
-#         config = json.load(config_file)
-#         API_KEY = config.get('api_key')
-# except FileNotFoundError:
-#     print("Error: config.json not found.")
-#     API_KEY = None
+base = base.rename(columns={"total_price": "total_price_br"})
 
-# if API_KEY:
-#     try:
-#         client = freecurrencyapi.Client(API_KEY)
-        
-#         # Fazer a solicitação para obter as moedas BRL e CAD
-#         result = client.currencies(currencies=['BRL', 'CAD'])
-#         print("Currency Data:", result)
+try:
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+        API_KEY = config.get('api_key')
+except FileNotFoundError:
+    print("Error: config.json not found.")
+    API_KEY = None
+
+if API_KEY:
+    try:
+        client = freecurrencyapi.Client(API_KEY)
+
+        result = client.latest(currencies=['BRL', 'USD'])
+        print("Currency Data:", result)
+ 
+        exchange_rate = result['data']['BRL']
+        print(f"Exchange Rate (BRL -> USD): {exchange_rate}")
     
-#     except freecurrencyapi.exceptions.FreecurrencyAPIException as e:
-#         print(f"API Error: {e}")
-#         exchange_rate = 1  
-#     except Exception as e:
-#         print(f"Unexpected Error: {e}")
-#         exchange_rate = 1  
-# else:
-#     print("Error: API_KEY not available. Please check your config.json.")
-#     exchange_rate = 1 
+    except Exception as e:  
+        print(f"API Error: {e}")
+        exchange_rate = 1  
+else:
+    print("Error: API_KEY not available. Please check your config.json.")
+    exchange_rate = 1 
 
-# base['total_price_us'] = base['total_price_br'] / exchange_rate
+if exchange_rate and isinstance(exchange_rate, (int, float)):
 
-# base.to_csv('results/fixed_order_full_information22.csv', index=False)
+    base['total_price_us'] = base['total_price_br'] / exchange_rate
+    print(base.head())
+else:
+    print("Error: Invalid exchange rate.")
 
-
-
-
+# Salvar os resultados
+base.to_csv('results/fixed_order_full_information.csv', index=False)
